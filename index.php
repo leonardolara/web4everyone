38733c8ca0cf1ecd6e7642439a2c19b0faab9760
Leonardo Lara Rodrigues <leonardo.lara.rodrigues@gmail.com>
2022-03-14 16:47:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Leonardo Lara Rodrigues</title>
+  <link type="text/css" rel="stylesheet" href="styles.css">
+</head>
+<body>
+<h1>Leonardo Lara Rodrigues PHP</h1>
+<pre>
+    LLL
+    LLL
+    LLL
+    LLL
+    LLL
+    LLLLLLLLLLLL
+    LLLLLLLLLLLL
+</pre>
+<?php
+    print hash('sha256','Leonardo Lara Rodrigues');
+?>
+</body>
+</html>
